257d71d2146e861e50095e7d320a8bcb7607828f
sofiagranda <cfm.10c.nicolasparedes@gmail.com>
2026-01-14 19:31:33 -0500

    registro

--- mi_app/views.py
+++ mi_app/views.py
@@ -3,8 +3,9 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 from .serializers.categoria_serializer import CategoriaSerializer
 from .serializers.cliente_serializer import ClienteSerializer
